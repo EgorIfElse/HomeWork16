1057c7254f6116f837921c622501467f747af5bd
user <user@WIN-PRBG4ODTMQN>
2024-11-30 23:03:03 +0300

    Добавьте файлы проекта.

---
+++ HomeWork16/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using Task1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int n = 5;
+        Tovar[] tovar = new Tovar[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Введите код товара");
+
+            int code = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите название товара");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите цену товара");
+            int price = Convert.ToInt32(Console.ReadLine());
+
+            tovar[i] = new Tovar() { Code = code, Name = name, Price = price };
+        }
+        JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+            WriteIndented = true
+        };
+        string jsonString = JsonSerializer.Serialize(tovar, options);
+        using (StreamWriter sw = new StreamWriter("../../../Products.json"))
+        {
+            sw.WriteLine(jsonString);
+        }
+
+    }
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string jsonString = String.Empty;
+        using (StreamReader sr = new StreamReader("../../../../Products.json"))
+        {
+            jsonString = sr.ReadToEnd();
+        }
+        Console.WriteLine(jsonString);
+        Tovar[] tovar = JsonSerializer.Deserialize<Tovar[]>(jsonString);
+        Tovar[] tovarMax = tovar[0];
+        foreach (Tovar t in tovar)
+        {
+            if (t.Price > tovarMax.Price)
+            {
+                tovarMax = t;
+            }
+        }
+        Console.WriteLine($"{tovarMax.Name}");
+    }
+}
+public class Tovar
+{
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+}
